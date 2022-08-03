9430b86cc4ae8a58c1197e9991e301d0a862514b
abilardo <don.abilardo@gmail.com>
2022-08-03 12:00:49 +0500

    Example006 max

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 10;
+int c = 6;
+int d = 4;
+int f = 18;
+
+int min = a;
+
+if (a < b) { min = b; }
+if (min < c) { min = c; }
+if (min < d) { min = d; }
+if (min < f) { min = f; }
+Console.WriteLine(min);
