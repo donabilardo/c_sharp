2a8a474744c055182b3af406bf9df3f7dc570a65
abilardo <don.abilardo@gmail.com>
2022-08-03 10:17:07 +0500

    Example004

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,8 @@
-﻿double numberA = 73;
-double numberB = 2;
+﻿//double a = new Random().Next(1, 10);
+Console.WriteLine("Введите первое число: ");
+double numberA = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double numberB = Int32.Parse(Console.ReadLine());
 double result = numberA / numberB;
+Console.Write("Результат деления: ");
 Console.WriteLine(result);
