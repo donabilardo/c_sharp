b48434b59b12ff9c2c50c94f945f3551a6d9d41a
abilardo <don.abilardo@gmail.com>
2022-08-11 07:00:27 +0500

    Недоделал

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int xa = 1, ya = 1; // Если позиция курсора не вмещается в консоль, то вываливается в ошибку
+//int xb = 1, yb = 30; //Первое значение в ширину, второе значение высота
+//int xc = 80, yc = 30;
+
+
+
+//Console.SetCursorPosition(xa, ya);
+//Console.WriteLine("xa");
+
+while (xa < 27)
+{
+    Console.SetCursorPosition(xa, ya);
+    Console.Write("xa");
+    xa++;
+    ya++;
+}
+System.Console.WriteLine("xszxzxzxxzxzxz");
+
