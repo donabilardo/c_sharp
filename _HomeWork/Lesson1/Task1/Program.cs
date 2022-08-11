4ec2ac7540a1515207993e6d89ebb29246eabfe4
abilardo <don.abilardo@gmail.com>
2022-08-11 14:01:08 +0500

    Lesoon 1 - Task 1

---
+++ _HomeWork/Lesson1/Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 ->  max = 7, min = 5
+//a = -9 b = -3 -> max = -3, min = -9
+
+
+
+Console.Clear();
+System.Console.Write("Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. a = 5; b = 7 ->  max = 7, min = 5");
+System.Console.WriteLine("");
+System.Console.WriteLine("Решение задачи: ");
+int a1 = 5; // Первое число
+int b1 = 7; // Второе число
+
+if (a1 > b1) {
+    System.Console.WriteLine("max = a1");
+    System.Console.WriteLine("min = b1");
+} else {
+    System.Console.WriteLine("max = b1");
+    System.Console.WriteLine("min = a1");
+}
+
+
+System.Console.WriteLine("");
+System.Console.WriteLine("");
+System.Console.WriteLine("");
+System.Console.WriteLine("");
+
+System.Console.Write("Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. a = -3; b = -9 ->  max = -3, min = -9");
+System.Console.WriteLine("");
+System.Console.WriteLine("Решение задачи: ");
+int a2 = -3; // Первое число
+int b2 = -9; // Второе число
+
+if (a2 > b2) {
+    System.Console.WriteLine("max = a2");
+    System.Console.WriteLine("min = b1");
+} else {
+    System.Console.WriteLine("max = b1");
+    System.Console.WriteLine("min = a1");
+}
+
+
