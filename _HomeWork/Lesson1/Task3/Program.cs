037ab92a37d969cca937f92c36fac726280af340
abilardo <don.abilardo@gmail.com>
2022-08-12 15:52:19 +0500

    Проверка числа на четность/не четность

---
+++ _HomeWork/Lesson1/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> Четное
+
+Console.Clear();
+int Prompt (string message) {
+    string ReadInt = System.Console.ReadLine(); //считываем значение
+    int result = int.Parse(ReadInt); //преобразуем в число
+    return result; //возвращает результат
+}
+
+System.Console.WriteLine("Программа проверяет является ли введенное число четным");
+System.Console.WriteLine("");
+int Num = Prompt("Введите число: "); 
+
+System.Console.WriteLine("");
+
+if (Num%2 == 0){
+    System.Console.WriteLine("Четное");
+}else{
+    System.Console.WriteLine("Не четное");
+}
