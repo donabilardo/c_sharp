4e460a9d2e7817366ed13ad732608c01abc583fe
abilardo <don.abilardo@gmail.com>
2022-08-13 16:42:19 +0500

    Доделал треугольник

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,20 +1,46 @@
 ﻿Console.Clear();
 
-int xa = 1, ya = 1; // Если позиция курсора не вмещается в консоль, то вываливается в ошибку
-//int xb = 1, yb = 30; //Первое значение в ширину, второе значение высота
+// Если позиция курсора не вмещается в консоль, то вываливается в ошибку
+//int xb = 1, yb = 30; //Первое значение в ширину, второе значение строки
 //int xc = 80, yc = 30;
-
-
-
 //Console.SetCursorPosition(xa, ya);
-//Console.WriteLine("xa");
 
-while (xa < 27)
+
+int xa = 40; //от левого 
+int ya = 1; //сверху
+
+int xb = 1; //от левого 
+int yb = 30; //сверху
+
+int xc = 80; //от левого 
+int yc = 30; //сверху
+
+int x = xa, y = xb;
+int count = 0;
+
+while (count < 10000)
 {
-    Console.SetCursorPosition(xa, ya);
-    Console.Write("xa");
-    xa++;
-    ya++;
+    int what = new Random().Next(0, 3);
+
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+
+    count++;
 }
-System.Console.WriteLine("xszxzxzxxzxzxz");
+
 
