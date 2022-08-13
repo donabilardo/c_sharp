1bc170d6f34ba126b2968b2f74b1a83ac0a24be2
abilardo <don.abilardo@gmail.com>
2022-08-13 19:14:46 +0500

    Поиск индекса массива по заданному числу

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+int findMaxNumber(int num1, int num2, int num3)  //Метод для поиска максимального числа
+{
+    int findMaxNumber = num1;
+    if (num2 > findMaxNumber) findMaxNumber = num2;
+    if (num3 > findMaxNumber) findMaxNumber = num3;
+    return findMaxNumber;
+}
+
+int[] ArrayNumber = { 19, 29, 43, 64, 75, 26, 17, 81, 29 };
+int N = ArrayNumber.Length; //длинна массива
+int ArrayIndex = 0; //Начальный индекс массива
+int findNumber = 29;
+
+
+while (ArrayIndex < N)
+{
+    if (findNumber == ArrayNumber[ArrayIndex])
+    {
+        System.Console.WriteLine(ArrayIndex);
+        break; //По условию задачи нужно найти первое совпавшее число, данный оператор прерывает выполнение
+    }
+    ArrayIndex++;
+}
+
+
+
+
+
+
+/* while (N < ArrayIndex)
+{
+    if (ArrayNumber[ArrayIndex] == findNumber)
+    {
+        System.Console.WriteLine(ArrayNumber[ArrayIndex]);
+    }
+    ArrayIndex = ArrayIndex + 1;
+
+}
+ */
