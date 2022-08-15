17abc87f55bdcbf68dc5dbf69af6d7ade55aa29d
abilardo <don.abilardo@gmail.com>
2022-08-15 11:50:46 +0500

    не работает

---
+++ Other_task/two_friend_and_dog/Program.cs
@@ -0,0 +1,32 @@
+﻿//Вычислить сколько раз собака пробежит от 1 друга ко второму, пока они двигаются на встречу к друг другу
+
+Console.Clear();
+
+
+int count = 0;
+int distance = 10000;
+int OneFriendSpeed = 1;
+int TwoFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+while (distance < 10000)
+{
+    if (friend == 1)
+    {
+        time = distance / (OneFriendSpeed + dogSpeed);
+        friend = 2;
+        System.Console.WriteLine(time + "первый");
+    }
+    else
+    {
+        time = distance / (TwoFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (OneFriendSpeed + TwoFriendSpeed) * time;
+    count++;
+}
+
+
+
