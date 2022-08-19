34765d01625038ced2d4fc24980bbe33bf63c4c8
abilardo <don.abilardo@gmail.com>
2022-08-19 17:12:26 +0500

    Поиск и замена указанных символов

---
+++ Example012_Method/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Дан текст. В тексте нужно все пробелы заменить черточками,
+//  маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+//  маленькими “с”.
+
+Console.Clear();
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, " +
+"ежели бы вас послали вместо нашего милого " +
+"Винценгероде, вы бы взяли приступом согласие " +
+"прусского короля. Вы так красноречивы. Выдадите мне чаю?";
+
+
+string convertText(string text, char oldValue, char newValue)
+{
+
+    string result = String.Empty;
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+
+        if (text[i] == oldValue)
+        {
+            result = result + $"{newValue}";
+        }
+        else
+        {
+            result = result + $"{text[i]}";
+        }
+
+    }
+
+    return result;
+}
+
+
+string newText = convertText(text, 'к', 'К');
+System.Console.WriteLine(newText);
