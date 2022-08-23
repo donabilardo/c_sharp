6481a675893315982abb687596603a907a4d1996
abilardo <don.abilardo@gmail.com>
2022-08-23 07:22:31 +0500

    Пятое задание

---
+++ _HomeWork/lesson5/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+//  [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message); //Выводим приглашение ко вводу
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput); //приводим к числу
+    return result; //возвращаем результат
+}
