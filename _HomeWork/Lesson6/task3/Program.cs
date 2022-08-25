399ae8129e97f50a693625ea07cc9a87ac9077d1
abilardo <don.abilardo@gmail.com>
2022-08-25 17:30:00 +0500

    Метод генерации и печати двумерного массива

--- _HomeWork/Lesson6/task3/Program.cs
+++ _HomeWork/Lesson6/task3/Program.cs
@@ -20,26 +20,41 @@ Console.Clear();
 int startNums = Prompt("Начало диапазона для генерации случайных чисел");
 Console.Clear();
 //int endNums = Prompt("Конец диапазона для генерации случайных чисел");
-Console.Clear();
- */
+Console.Clear(); */
+
 
 
 
 int[,] Random2Dim(int Rows, int Collumns, int startNums, int endNums) // метод для динамической генерации двумерного массива заполеннного случайными числами
 {
-    int[,] array = new int[Rows, Collumns]; //обьявляем массив
-    int arrLength0 = 0; //счетчик для длинны массива
-    int arrLength1 = 1; //счетчик для длинны массива
+    int[,] array = new int[Rows, Collumns]; //обьявляем массив Rows - строки, Collumns - колонки
 
-    for (int row = 0; row < array.GetLength(arrLength0); row++)
+
+    for (int row = 0; row < array.GetLength(0); row++) //генерация строки
     {
-        for (int col = 0; col < array.GetLength(arrLength1); col++){
-            array[row,col] = new Random().Next(startNums,endNums);
+        for (int col = 0; col < array.GetLength(1); col++) //генерация колонки
+        {
+            array[row, col] = new Random().Next(startNums, endNums); //заполнение ячеек массива случайными числами
         }
     }
-return array;
+    return array; //возвращаем массив
+}
+
+
+void PrintArray(int[,] array)  // метод для печати массива в консоль
+{
+    for (int row = 0; row < array.GetLength(0); row++) // пока меньше длинны массива
+    {
+        for (int col = 0; col < array.GetLength(1); col++) // пока меньше длинны массива
+        {
+            System.Console.Write($"{array[row, col]} "); //вывод данных в 1 строку
+            
+        }
+        System.Console.WriteLine(""); //пустая строка для переноса
+    }
 }
 
 
 
-
+int[,] RandomArr = Random2Dim(4, 4, 1, 9); //генерация массива 1 параметр строка, 2 колонки, 3 начало и 4 конец для диапазона случайных чисел
+PrintArray(RandomArr); // печать в консоль
