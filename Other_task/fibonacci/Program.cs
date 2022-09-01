7545e380a2613f149e7f09d76d98a6687f2d4bc3
abilardo <don.abilardo@gmail.com>
2022-09-01 16:18:56 +0500

    Число фибоначи

---
+++ Other_task/fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int Fibonacci (int n){
+    if (n == 1 || n ==2) {
+        return 1;
+    } else {
+        return Fibonacci(n-1) + Fibonacci(n-2);
+    }
+}
+
+
+for (int i =1; i <100; i++){
+    System.Console.WriteLine($" Число {i} {Fibonacci(i)}");
+}
