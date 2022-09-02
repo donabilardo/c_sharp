158c36c3f00e5b557fb161b41ffeca15d4e22961
abilardo <don.abilardo@gmail.com>
2022-09-02 11:05:55 +0500

    Проверка существования треугольника

---
+++ Other_task/razvorot_massiva/Program.cs
@@ -0,0 +1,37 @@
+﻿//  Напишите программу, которая перевернёт одномерный массив 
+//  (последний элемент будет на первом месте, 
+//  а первый - на последнем и т.д.)
+//  [1 2 3 4 5] -> [5 4 3 2 1]
+//  [6 7 3 6] -> [6 3 7 6]
+
+
+Console.Clear();
+
+
+int[] matrix = new int[] { 1, 3, 5, 4, 8, 2, 1, 5, 9 };
+
+int[] ReverArray(int[] matrix)
+{
+
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        int temp = matrix[i];
+        matrix[i] = matrix[matrix.Length / 2 - 1 - i];
+        matrix[matrix.Length - 1 - i] = temp;
+    }
+    return matrix;
+}
+
+void PrintArray(int[] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        System.Console.WriteLine($"index {i} {matrix[i]}");
+    }
+}
+
+
+PrintArray(matrix);
+ReverArray(matrix);
+System.Console.WriteLine("REVERT");
+PrintArray(matrix);
---
+++ Other_task/triangle/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход три числа
+// и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message); //Выводим приглашение ко вводу
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput); //приводим к числу
+    return result; //возвращаем результат
+}
+
+bool Triangle(int num1, int num2, int num3)
+{
+    if (num1 + num2 > num3 && num2 + num3 > num1 && num3 + num1 > num2)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void Checktriangle(bool result)
+{
+    if (result == true)
+    {
+        System.Console.WriteLine("Треугольник существует");
+    }
+    else
+    {
+        System.Console.WriteLine("Треугольника НЕ существует");
+    }
+}
+
+
+
+
+int a = Prompt("1 сторона треугольника");
+int b = Prompt("2 сторона треугольника");
+int c = Prompt("3 сторона треугольника");
+
+
+
+Checktriangle(Triangle(a, b, c));
