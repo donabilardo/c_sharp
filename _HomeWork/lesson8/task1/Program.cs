7adc1aad74b83ffae4065ed9dd09ef4a0f727a32
abilardo <don.abilardo@gmail.com>
2022-09-04 17:29:52 +0500

    Сортировка одномерного массива (пока одномерного)

---
+++ _HomeWork/lesson8/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Задача 1: Задайте двумерный массив. 
+//  Напишите программу, которая упорядочит 
+//  по убыванию элементы каждой строки двумерного массива.
+
+/* Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+
+Console.Clear();
+
+
+
+int[] matrix = new int[] { 1, 4, 7, 2 };
+
+
+for (int i = 0; i < matrix.Length; i++)
+{
+    for (int j = 0; j < matrix.Length - 1; j++)
+    {
+        if (matrix[j] > matrix[j + 1])
+        {
+            int tmp = matrix[j];
+            matrix[j] = matrix[j + 1];
+            matrix[j + 1] = tmp;
+        }
+    }
+
+    System.Console.Write($"{matrix[i]}\t");
+}
+
+
+
