72bf083c489547fc946d876e5745c55b43a52fa6
abilardo <don.abilardo@gmail.com>
2022-09-05 12:14:16 +0500

    Сортировка пузырьком

--- _HomeWork/lesson8/task1/Program.cs
+++ _HomeWork/lesson8/task1/Program.cs
@@ -14,33 +14,87 @@
 
 Console.Clear();
 
+#region  Запрос данных у пользователя, наполнение массива, печать массива
 
-
-/* int[] matrix = new int[] { 1, 4, 7, 2 };
-
-
-for (int i = 0; i < matrix.Length; i++)
+int Prompt(string message)//метод получения данных от пользователя
 {
-    for (int j = 0; j < matrix.Length - 1; j++)
+    System.Console.WriteLine(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+int[,] CreateArray(int row, int col) // создание пустого массива
+{
+    int[,] array = new int[row, col];
+    return array;
+}
+
+void FillArrayRandomNum(int[,] array) // заполнение массива случайными числами
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        if (matrix[j] < matrix[j + 1])
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            int tmp = matrix[j];
-            matrix[j] = matrix[j + 1];
-            matrix[j + 1] = tmp;
+            array[i, j] = new Random().Next(0, 9);
         }
     }
 }
 
-
-for (int i = 0; i < matrix.Length; i++)
+void PrintArray(int[,] array) // печать массива
 {
-    System.Console.Write($"{matrix[i]}\t");
-} */
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($" {array[i, j]} ");
+        }
+        System.Console.WriteLine(String.Empty);
+    }
 
-int[,] matrix = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+}
+
+#endregion
 
 
 
+#region  Сортировка методом "пузырька"
 
-for (int i = 0; i)
+int[,] BubleSortArray(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            bool sort = true;
+            for (int j = 0; j < matrix.GetLength(1) - 1 - k; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int tmp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = tmp;
+                    sort = false;
+                }
+            }
+            if (sort) break;
+        }
+    }
+
+    return matrix;
+}
+
+#endregion
+
+
+int[,] matrix = CreateArray(Prompt("Введите число строк:"), Prompt("введите число колонок:"));
+FillArrayRandomNum(matrix);
+System.Console.WriteLine("Сгенерирован массив:");
+PrintArray(matrix);
+System.Console.WriteLine(String.Empty);
+System.Console.WriteLine("Отсортированный массив:");
+BubleSortArray(matrix);
+PrintArray(matrix);
+
+
